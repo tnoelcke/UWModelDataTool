8e73256a1a97cf8c812c175d127cc089af1a6e0e
tnoelcke <noelcket@oregonstate.edu>
2019-09-20 23:26:37 -0700

    feat(data tools): created blank data tools

---
+++ UWDataTool/DataManager/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ UWDataTool/UWDataTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UWDataTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
