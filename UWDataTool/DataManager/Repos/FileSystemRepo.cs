54a3b93b480e57f0aa97098ddb990e865f491552
tnoelcke <noelcket@oregonstate.edu>
2020-03-07 20:42:35 -0800

    feat(file-io): started working on file io drivers

---
+++ UWDataTool/DataManager/Repos/FileSystemRepo.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace DataManager.Repos
+{
+    class FileSystemRepo
+    {
+        private const string BASE_STORAGE_LOCATION = "./App_Data";
+
+        private string storageLocation;
+
+        private static FileSystemRepo _instnace;
+
+        public FileSystemRepo(string directory)
+        {
+            //TODO: Get this location from configuration.
+            storageLocation = Path.Combine(BASE_STORAGE_LOCATION, directory);
+        }
+
+        public Stream ReadFile() {
+            return null;
+        }
+
+        public void WriteFile(Stream file)
+        {
+
+        }
+
+        public static void DeleteFile()
+        {
+
+        }
+
+        public void writeFilies(IList<Stream> files)
+        {
+            foreach(Stream file in files)
+            {
+                this.WriteFile(file);
+            }
+        }
+    }
+}
